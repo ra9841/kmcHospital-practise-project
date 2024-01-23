0fd15e0688c550241d86fcb2467bb718a0ce5dec
Rabin <rabin.shrestha7@gmail.com>
2024-01-23 10:27:13 -0800

    all commit

--- src/main/java/com/rabin/hospitalpractiseproject/service/PatientService.java
+++ src/main/java/com/rabin/hospitalpractiseproject/service/PatientService.java
@@ -2,6 +2,10 @@ package com.rabin.hospitalpractiseproject.service;
 
 import com.rabin.hospitalpractiseproject.dto.PatientDto;
 
+import java.util.List;
+
 public interface PatientService {
     PatientDto savingPatentRecordIntoDataBase(PatientDto patientDto);
+
+    List<PatientDto> gettingAllListOfRecord();
 }
