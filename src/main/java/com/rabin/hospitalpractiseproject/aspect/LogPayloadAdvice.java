ed0b9f2ad2d183673e6f041db9358419df1e1941
Rabin <rabin.shrestha7@gmail.com>
2024-01-30 20:08:00 -0800

    pagination on patient record

--- src/main/java/com/rabin/hospitalpractiseproject/aspect/LogPayloadAdvice.java
+++ src/main/java/com/rabin/hospitalpractiseproject/aspect/LogPayloadAdvice.java
@@ -1,4 +1,4 @@
-package com.rabin.hospitalpractiseproject.advice;
+package com.rabin.hospitalpractiseproject.aspect;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
